e901ab49217a6478d494a97a6c62cab2ddabba39
oyvindjr-sigvaldsen <oyvindjr.sigvaldsen@me.com>
2019-10-13 23:08:05 +0200

    Directory management

--- source-code/backend/php/generate_graphs.php
+++
@@ -1,4 +0,0 @@
-<?php
-shell_exec("cd ../interactive-graphs/regression-prediction/; python regression_prediction.py");
-shell_exec("cd ../interactive-graphs/historical-close-prices/; python historical_close_prices.py");
-?>
